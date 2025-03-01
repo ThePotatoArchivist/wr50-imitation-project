8926535d2461fdcf22e64d844d523ee5bb42dd37
Josiah Fu <josiahjf@uci.edu>
2025-03-01 02:05:04 -0800

    Test map working

---
+++ src/lib/TestMap.svelte
@@ -0,0 +1,70 @@
+<script lang="ts" module>
+    declare global {
+        interface Window {
+            initialize: () => void
+        }
+    }
+</script>
+
+<script lang="ts">
+    import { Map, StreetViewPanorama } from './maps'
+    import { onMount } from 'svelte'
+
+    let mapElement: HTMLElement
+    let map: Map
+    let panorama: StreetViewPanorama
+
+    const locations = [
+        { lat: 42.345573, lng: -71.098326 },
+        { lat: 42.34542,  lng: -71.098238 },
+        { lat: 42.3455569, lng: -71.0977137 },
+    ]
+
+    const defaultLocation = locations[0]
+    let location = $state(defaultLocation)
+
+    onMount(() => {
+        map = new Map(
+            mapElement,
+            {
+                center: defaultLocation,
+                zoom: 14,
+            }
+        )
+        panorama = new StreetViewPanorama(
+            mapElement,
+            {
+                position: defaultLocation,
+                pov: {
+                    heading: 34,
+                    pitch: 10,
+                },
+            }
+        )
+
+        map.setStreetView(panorama)
+    })
+
+    $effect(() => {
+        $inspect(location)
+        panorama.setPosition(location)
+    })
+</script>
+
+<div bind:this={mapElement}></div>
+
+<div class="options">
+    {#each locations as loc}
+        <label>
+            <input type="radio" bind:group={location} value={loc} />
+            {JSON.stringify(loc)}
+        </label>
+    {/each}
+</div>
+
+<style>
+    .options {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
--- src/lib/maps.ts
+++ src/lib/maps.ts
@@ -1,8 +1,10 @@
 import { Loader } from '@googlemaps/js-api-loader'
 
 const loader = new Loader({
-    apiKey: import.meta.env.API_KEY,
+    apiKey: import.meta.env.VITE_API_KEY,
     version: 'weekly',
 })
 
-export const { Map } = await loader.importLibrary('maps')
+export const [{ Map }, { StreetViewPanorama }] = await Promise.all([loader.importLibrary('maps'), loader.importLibrary('streetView')])
+export type Map = InstanceType<typeof Map>
+export type StreetViewPanorama = InstanceType<typeof StreetViewPanorama>
