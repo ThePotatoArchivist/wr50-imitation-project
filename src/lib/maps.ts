45015acadbae1460e841583817996c9a08e4a7ae
Josiah Fu <josiahjf@uci.edu>
2025-03-01 01:02:22 -0800

    Fix types, configure prettier

---
+++ src/lib/maps.ts
@@ -0,0 +1,8 @@
+import { Loader } from '@googlemaps/js-api-loader';
+
+const loader = new Loader({
+    apiKey: import.meta.env.API_KEY,
+    version: "weekly",
+});
+  
+export const { Map } = await loader.importLibrary('maps')
